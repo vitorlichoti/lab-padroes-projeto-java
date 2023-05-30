91fc717c40d57814fa41164533f68c5719f6e4fa
Vitor Lichoti <vitorlichoti@gmail.com>
2023-05-29 21:37:32 -0400

    Adiciona modificações no desafio

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -13,30 +13,30 @@ import one.digitalinnovation.gof.strategy.Robo;
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 		// Singleton
-		
+
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
 		lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
-		
+
 		SingletonEager eager = SingletonEager.getInstancia();
 		System.out.println(eager);
 		eager = SingletonEager.getInstancia();
 		System.out.println(eager);
-		
+
 		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
 		System.out.println(lazyHolder);
 		lazyHolder = SingletonLazyHolder.getInstancia();
 		System.out.println(lazyHolder);
-		
+
 		// Strategy
-		
+
 		Comportamento defensivo = new ComportamentoDefensivo();
 		Comportamento normal = new ComportamentoNormal();
 		Comportamento agressivo = new ComportamentoAgressivo();
-		
+
 		Robo robo = new Robo();
 		robo.setComportamento(normal);
 		robo.mover();
@@ -47,11 +47,11 @@ public class Test {
 		robo.mover();
 		robo.mover();
 		robo.mover();
-		
+
 		// Facade
-		
+
 		Facade facade = new Facade();
-		facade.migrarCliente("Venilton", "14801788");
+		facade.migrarCliente("Vitor", "79750000");
 	}
 
 }
